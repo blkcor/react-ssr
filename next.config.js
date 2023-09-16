6a81b5d4aac99ebaa64a9d232d070b24c8ffded3
blkcor <1029129867@qq.com>
2023-09-16 10:32:03 +0800

    chore: add husky and commitlint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const path = require('path')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-/** @ */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
